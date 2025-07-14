f33d063ea1e7d9706eda374296c83f07efe200d7
yikoyu <2282373181@qq.com>
2025-07-14 10:23:38 +0800

    chore: 精简版本antd，包括精简icon

--- packages/@core/base/icons/src/index.ts
+++ packages/@core/base/icons/src/index.ts
@@ -2,6 +2,8 @@ export * from './create-icon';
 
 export * from './lucide';
 
+export * from './register-icon';
+
 export type { IconifyIcon as IconifyIconStructure } from '@iconify/vue';
 export {
   addCollection,
